859a40a2038a967b2807150159f9927f81046048
agrawalsrijan <47100634+agrawalsrijan@users.noreply.github.com>
2020-09-30 11:35:23 +0530

    Adding Player component

---
+++ src/Player.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+
+// import css styles
+import "./Player.css"
+
+import Sidebar from './Sidebar'
+import Body from './Body'
+import Footer from './Footer'
+
+function Player({ spotify }) {
+    return (
+        <div className="player">
+            <div className="player__body">
+                
+                {/* Sidebar */}
+                <Sidebar/>
+
+
+                {/* Body */}
+                <Body/>
+                
+            </div>
+            {/* Footer */}
+            <Footer/>
+        </div>
+    )
+}
+
+export default Player  
