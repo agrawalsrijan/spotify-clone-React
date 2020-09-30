1432a8a4867131baa6f8bc4eb39dcb53c4ac5342
agrawalsrijan <47100634+agrawalsrijan@users.noreply.github.com>
2020-09-30 15:07:05 +0530

    implementing functionlaity to set current playing tracks

--- src/App.js
+++ src/App.js
@@ -1,16 +1,83 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+
+import {useDataLayerValue} from "./DataLayer"
+
+import SpotifyWebApi from "spotify-web-api-js"
+import { getTokenFromUrl } from './spotify';
 
 import './App.css';
 
 // Components import
-import Login from "./Login"
+import Login from "./Login";
+import Player from "./Player"
+
+const spotify = new SpotifyWebApi()
 
 function App() {
+  
+  const [{ user, token }, dispatch] = useDataLayerValue();
+
+  // run code based on given condition
+  useEffect(() => {
+    const hash  = getTokenFromUrl();
+    window.location.hash= "";
+
+    const _token = hash.access_token;
+
+    if (_token) {
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token
+      })
+
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then(user => {
+        
+        dispatch({
+          type: "SET_USER",
+          user: user
+        })
+
+      })
+
+      spotify.getMyTopArtists().then((response) =>
+        dispatch({
+          type: "SET_TOP_ARTISTS",
+          top_artists: response,
+        })
+      );
+
+      dispatch({
+        type: "SET_SPOTIFY",
+        spotify: spotify,
+      });
+
+      spotify.getUserPlaylists().then(playlists => {
+        dispatch({
+          type: "SET_PLAYLISTS",
+          playlists: playlists
+        })
+      })
+
+      spotify.getPlaylist("37i9dQZEVXcRg4VdFCH2SM").then(response => {
+        dispatch({
+          type: "SET_DISCOVER_WEEKLY",
+          discover_weekly: response,
+        })
+      })
+
+    }
+
+    
+  }, [token, dispatch])
+
+
+
   return (
     <div className="app">
-      {/* Spotify Logo */}
-      {/* Login with spotify button */}
-      <Login/>
+      {!token && <Login />}
+      {token && <Player spotify={spotify} />}
     </div>
   );
 }
--- src/reducer.js
+++ src/reducer.js
@@ -1,36 +1,68 @@
+import { findAllByDisplayValue } from "@testing-library/react";
+
 export const initialState = {
     user: null,
     playlists: [],
+    spotify: null,
+    discover_weekly: null,
+    top_artists: null,
     playing: false,
     item: null,
-    token: "BQChhTLA-w9YhRrQ9sHgZqdUr2vedhafjmZtZ1Y_XiB_pfG3YETAFUb-dvNpTuZqVc4z2g-wHylxsAPyOX3rmimiUgwsSpCGbvf4q-dBy536JakH-mmgEPnUN1gsble_XESdiNy17xq07br2ZeOIIqUbTcA-pd4TgPRE3jtJtytgjmRze594"
-}
+};
 
 const reducer = (state, action) => {
     console.log(action);
+    switch (action.type) {
+        case "SET_USER":
+            return {
+                ...state,
+                user: action.user,
+            };
 
-    switch(action.type) {
-        
+        case "SET_PLAYING":
+            return {
+                ...state,
+                playing: action.playing,
+            };
+
+        case "SET_ITEM":
+            return {
+                ...state,
+                item: action.item,
+            };
+
+        case "SET_DISCOVER_WEEKLY":
+            return {
+                ...state,
+                discover_weekly: action.discover_weekly,
+            };
+
+        case "SET_TOP_ARTISTS":
+            return {
+                ...state,
+                top_artists: action.top_artists,
+            };
+
         case "SET_TOKEN":
             return {
                 ...state,
-                token: action.token
-            }
+                token: action.token,
+            };
 
-        case "SET_USER" :
+        case "SET_SPOTIFY":
             return {
                 ...state,
-                user: action.user
-            }
-        case "SET_PLAYLISTS" :
+                spotify: action.spotify,
+            };
+
+        case "SET_PLAYLISTS":
             return {
                 ...state,
-                playlists: action.playlists
-            }
-        default :
-            return state
+                playlists: action.playlists,
+            };
+        default:
+            return state;
     }
-}
+};
 
-
-export default reducer
+export default reducer;
